3cce053e177b81dcf755f4296a39c06f2e15aaeb
Nurizki <nutrisi1udin@gmail.com>
2022-10-02 20:18:42 +0700

    tugas 19 js
    tugas niomic

---
+++ tugas19-1.js
@@ -0,0 +1,3 @@
+var kata1 = 'Semangat...'
+
+console.log(kata1.repeat(5))
---
+++ tugas19-2.js
@@ -0,0 +1,17 @@
+function bintang(n){
+    var b = '*****'
+
+    ulang = b.repeat(n)
+
+    return ulang
+}
+
+console.log(bintang(1))
+console.log(bintang(3))
+console.log(bintang(6))
+console.log(bintang(9))
+console.log(bintang(12))
+console.log(bintang(9))
+console.log(bintang(6))
+console.log(bintang(3))
+console.log(bintang(1))
